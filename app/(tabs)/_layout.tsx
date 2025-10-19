db6a74774402fd56d98fa7b7010f627475c2cb2d
Shiyas Mohammed <shiyasmohd.official@gmail.com>
2025-10-19 22:06:29 +0530

    fix: navbar height

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -16,7 +16,7 @@ export default function TabLayout() {
           backgroundColor: '#FFFFFF',
           borderTopWidth: 1,
           borderTopColor: '#E5E7EB',
-          height: 60,
+          height: 80,
           paddingBottom: 8,
           paddingTop: 8,
         },
