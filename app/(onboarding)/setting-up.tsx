3aa3813ca0256f847a2171124249b5828816ef84
unniznd <au6695@gmail.com>
2025-10-23 17:38:35 +0530

    Added keypair generation and moved implementation to setting up page

--- app/(onboarding)/setting-up.tsx
+++ app/(onboarding)/setting-up.tsx
@@ -1,5 +1,13 @@
+import 'react-native-get-random-values';
+
+import { Keypair } from '@solana/web3.js';
+import * as bip39 from 'bip39';
+import slip10 from 'micro-key-producer/slip10.js';
+import * as Keychain from 'react-native-keychain';
+
 import { Text } from "@/components/ui/text";
 import { useAuth } from "@/lib/contexts/AuthContext";
+import { apiService } from '@/lib/services/api';
 import { storage } from "@/lib/storage/storage";
 import { router } from "expo-router";
 import React, { useEffect, useState } from "react";
@@ -20,47 +28,104 @@ export default function SettingUpScreen() {
   const [status, setStatus] = useState("Setting up account...");
 
   useEffect(() => {
-    const setupAccount = async () => {
-      try {
-        setStatus("Finalizing your profile...");
-
-        // TODO: Make an setup required
-
-        // Simulate final setup process
-        await new Promise((resolve) => setTimeout(resolve, 1500));
-
-        // Complete onboarding locally
-        await completeOnboarding();
-        try {
-          await storage.saveLatestTransactions([]);
-          console.log("Transactions fetched and stored for returning user");
-        } catch (error) {
-          console.warn(
-            "Failed to fetch transactions for returning user:",
-            error
-          );
-          // Continue even if transaction fetch fails
-        }
-        // Navigate to home
-        router.replace("/(tabs)");
-      } catch (err) {
-        console.error("Error completing setup:", err);
-        setStatus("Setup complete!");
-
-        // Complete onboarding locally even if backend fails
-        try {
-          await completeOnboarding();
-        } catch (localError) {
-          console.error("Error completing local onboarding:", localError);
-        }
-
-        // Navigate to home
-        router.replace("/(tabs)");
-      }
-    };
-
     setupAccount();
   }, [user?.user_id, completeOnboarding]);
+
+
+  const generatePublicKeyandStoreSeedPhrase = async () => {
+    try {
+      // Store the seed phrase securely with biometric protection
+      const options = {
+        accessControl: Keychain.ACCESS_CONTROL.BIOMETRY_ANY,
+        accessible: Keychain.ACCESSIBLE.WHEN_PASSCODE_SET_THIS_DEVICE_ONLY,
+        authenticationType: Keychain.AUTHENTICATION_TYPE.BIOMETRICS,
+        securityLevel: Keychain.SECURITY_LEVEL.SECURE_HARDWARE,
+        service: 'bip39_seed_phrase',
+      };
+      // Generate a 128-bit (12-word) BIP39 seed phrase
+      let mnemonic = bip39.generateMnemonic();
+
+      // Store the mnemonic in keychain
+      const result = await Keychain.setGenericPassword(
+        'bip39_seed_phrase',
+        mnemonic,
+        options
+      );
+
+      // Verify storage was successful
+      if (result) { 
+        const seed = bip39.mnemonicToSeedSync(mnemonic);
+        const hdMaster = slip10.fromMasterSeed(new Uint8Array(seed));
+        const derived = hdMaster.derive("m/44'/501'/0'/0'");
+        const privateKey = Uint8Array.from(derived.privateKey);
+        const keypair = Keypair.fromSeed(privateKey);
+        const publicKey = keypair.publicKey.toString();
+        return publicKey;
+      }
+      return null;
+    } catch (error) {
+      console.error('Error generating or storing seed phrase:', error);
+      return null;
+    }
+  }
+
+  const setupAccount = async () => {
+    try {
+      if (!user?.user_id) {
+        return;
+      }
+      setStatus("Finalizing your profile...");
+
+      const publicKey = await generatePublicKeyandStoreSeedPhrase();
+      if(!publicKey){
+        router.push('/(onboarding)/avatar')
+        return;
+      }
+      const response = await apiService.updateUser(user.user_id, {
+        primary_address: publicKey,
+        wallet_address: [publicKey,]
+      })
+      if (!response.success) {
+        router.push('/(onboarding)/avatar')
+        return;
+      }
+      try{
+        await storage.savePublicKey(publicKey);
+      }catch(err){
+        console.log(err)
+      }
+      
+
+      // Complete onboarding locally
+      await completeOnboarding();
+      try {
+        await storage.saveLatestTransactions([]);
+        console.log("Transactions fetched and stored for returning user");
+      } catch (error) {
+        console.warn(
+          "Failed to fetch transactions for returning user:",
+          error
+        );
+        // Continue even if transaction fetch fails
+      }
+      // Navigate to home
+      router.replace("/(tabs)");
+    } catch (err) {
+      console.error("Error completing setup:", err);
+      setStatus("Setup complete!");
+
+      // Complete onboarding locally even if backend fails
+      try {
+        await completeOnboarding();
+      } catch (localError) {
+        console.error("Error completing local onboarding:", localError);
+      }
+
+      // Navigate to home
+      router.replace("/(tabs)");
+    }
+  };
+
 
   return (
     <SafeAreaView
