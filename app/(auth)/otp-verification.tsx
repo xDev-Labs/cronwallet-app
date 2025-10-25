2888fff27c54bafc9eed37313240bec6fe0ae9c2
unniznd <au6695@gmail.com>
2025-10-25 14:54:02 +0530

    Added expo notification token generation and saving

--- app/(auth)/otp-verification.tsx
+++ app/(auth)/otp-verification.tsx
@@ -7,6 +7,9 @@ import { storage } from "@/lib/storage/storage";
 import { fetchAndStoreUserTransactions } from "@/lib/utils/transactionService";
 import { mapBackendUserToUser } from "@/lib/utils/userMapping";
 import auth from "@react-native-firebase/auth";
+import Constants from 'expo-constants';
+import * as Device from 'expo-device';
+import * as Notifications from 'expo-notifications';
 import { useLocalSearchParams, useRouter } from "expo-router";
 import { useEffect, useState } from "react";
 import {
@@ -68,6 +71,52 @@ export default function OTPVerificationScreen() {
     }
   };
 
+  function handleRegistrationError(errorMessage: string) {
+    alert(errorMessage);
+    throw new Error(errorMessage);
+  }
+
+  async function registerForPushNotificationsAsync() {
+    if (Platform.OS === 'android') {
+      await Notifications.setNotificationChannelAsync('default', {
+        name: 'default',
+        importance: Notifications.AndroidImportance.MAX,
+        vibrationPattern: [0, 250, 250, 250],
+        lightColor: '#FF231F7C',
+      });
+    }
+
+    if (Device.isDevice) {
+      const { status: existingStatus } = await Notifications.getPermissionsAsync();
+      let finalStatus = existingStatus;
+      if (existingStatus !== 'granted') {
+        const { status } = await Notifications.requestPermissionsAsync();
+        finalStatus = status;
+      }
+      if (finalStatus !== 'granted') {
+        handleRegistrationError('Permission not granted to get push token for push notification!');
+        return;
+      }
+      const projectId = Constants?.expoConfig?.extra?.eas?.projectId;
+      if (!projectId) {
+        handleRegistrationError('Project ID not found');
+      }
+      try {
+        const pushTokenString = (
+          await Notifications.getExpoPushTokenAsync({
+            projectId,
+          })
+        ).data;
+        console.log(pushTokenString);
+        return pushTokenString;
+      } catch (e: unknown) {
+        handleRegistrationError(`${e}`);
+      }
+    } else {
+      handleRegistrationError('Must use physical device for push notifications');
+    }
+  }
+
   const handleVerify = async () => {
     if (otp.length !== 6 || isVerifying) return;
 
@@ -90,11 +139,23 @@ export default function OTPVerificationScreen() {
         const response = await apiService.createUser(fullPhoneNumber);
 
         if (response.success && response.data) {
-          console.log("Backend response:", response.data);
+          console.log("Backend response:", response.data);          
 
           // Handle different response structures
           const userData = response.data.user || response.data;
           console.log("User data to map:", userData);
+
+          let token = null;
+          try{
+            token = await registerForPushNotificationsAsync();
+          } catch(e){
+            console.log("Registering notification failed");
+          }
+
+          const updatedUserData = await apiService.updateUser(userData.user_id,{
+            expo_push_token: token
+          });
+          console.log("Updated user data:", updatedUserData);
 
           // Map backend response to our user model
           const mappedUserData = mapBackendUserToUser(userData);
