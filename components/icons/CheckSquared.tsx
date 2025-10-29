394506adb76bbf26b2ef6e038bc1403054fa685e
Shiyas Mohammed <83513144+shiyasmohd@users.noreply.github.com>
2025-10-29 13:38:58 +0530

    feat: handle payment success

---
+++ components/icons/CheckSquared.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import Svg, { Path, SvgProps } from 'react-native-svg';
+
+export const CheckSquared = ({ color = 'currentColor', size = 24, ...props }: SvgProps & { size?: number }) => (
+    <Svg
+        width={size}
+        height={size}
+        viewBox="0 0 24 24"
+        fill={color}
+        stroke="none"
+        strokeWidth={2}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        {...props}
+    >
+        <Path stroke="none" d="M0 0h24v24H0z" fill="none" />
+        <Path d="M18.333 2c1.96 0 3.56 1.537 3.662 3.472l.005 .195v12.666c0 1.96 -1.537 3.56 -3.472 3.662l-.195 .005h-12.666a3.667 3.667 0 0 1 -3.662 -3.472l-.005 -.195v-12.666c0 -1.96 1.537 -3.56 3.472 -3.662l.195 -.005h12.666zm-2.626 7.293a1 1 0 0 0 -1.414 0l-3.293 3.292l-1.293 -1.292l-.094 -.083a1 1 0 0 0 -1.32 1.497l2 2l.094 .083a1 1 0 0 0 1.32 -.083l4 -4l.083 -.094a1 1 0 0 0 -.083 -1.32z" />
+    </Svg>
+);
